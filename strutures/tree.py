c7d04fa883216c640537b4358f1c2e79caabd5e4
littlecastrum <littlecastrum@gmail.com>
2019-06-24 15:53:35 -0400

    new structures

--- strutures/tree.py
+++ strutures/tree.py
@@ -1,4 +1,4 @@
-from stack import Stack
+from queue import Queue
 
 class Node:
     def __init__(self, value=None):
@@ -40,115 +40,55 @@ class Tree:
     def get_root(self):
         return self.root
 
-class State(object):
-    def __init__(self,node):
-        self.node = node
-        self.visited_left = False
-        self.visited_right = False
-        
-    def get_node(self):
-        return self.node
-    
-    def get_visited_left(self):
-        return self.visited_left
-    
-    def get_visited_right(self):
-        return self.visited_right
-    
-    def set_visited_left(self):
-        self.visited_left = True
-        
-    def set_visited_right(self):
-        self.visited_right = True
-        
-    def __repr__(self):
-        s = f"""{self.node}
-visited_left: {self.visited_left}
-visited_right: {self.visited_right}
-        """
-        return s
-
-def pre_order_with_stack_buggy(tree):
+def pre_order(tree):
     visit_order = list()
-    stack = Stack()
-    node = tree.get_root()
-    stack.push(node)
-    node = stack.top()
-    visit_order.append(node.get_value())
-    count = 0
-    loop_limit = 7
-    while(node and count < loop_limit): 
-        print(f"""
-            loop count: {count}
-            current node: {node}
-            stack:
-            {stack}
-        """)
-        count +=1
-        if node.has_left_child():
-            node = node.get_left_child()
-            stack.push(node)
-            # using top() is redundant, but added for clarity
-            node = stack.top() 
-            visit_order.append(node.get_value())
-            
-        elif node.has_right_child():
-            node = node.get_right_child()
-            stack.push(node)
-            node = stack.top()
-            visit_order.append(node.get_value())
+    root = tree.get_root()
 
-        else:
-            stack.pop()
-            if not stack.is_empty():
-                node = stack.top()
-            else:
-                node = None
+    def traverse(node):
+        if node:
+            visit_order.append(node.get_value())
+            traverse(node.get_left_child())
+            traverse(node.get_right_child()) 
+
+    traverse(root)
+    return visit_order
+def in_order(tree):
+    visit_order = list()
+    root = tree.get_root()
+
+    def traverse(node):
+        if node:
+            traverse(node.get_left_child())
+            visit_order.append(node.get_value())
+            traverse(node.get_right_child()) 
+
+    traverse(root)
     return visit_order
 
-def pre_order_with_stack(tree, debug_mode=False):
+def post_order(tree):
     visit_order = list()
-    stack = Stack()
+    root = tree.get_root()
+
+    def traverse(node):
+        if node:
+            traverse(node.get_left_child())
+            traverse(node.get_right_child()) 
+            visit_order.append(node.get_value())
+
+    traverse(root)
+    return visit_order
+
+def bfs(tree):
+    visit_order = list()
+    q = Queue()
     node = tree.get_root()
-    visit_order.append(node.get_value())
-    state = State(node)
-    stack.push(state)
-    count = 0
-    while(node):
-        if debug_mode:
-            print(f"""
-loop count: {count}
-current node: {node}
-stack:
-{stack}
-            """)
-        count +=1
-        if node.has_left_child() and not state.get_visited_left():
-            state.set_visited_left()
-            node = node.get_left_child()
-            visit_order.append(node.get_value())
-            state = State(node)
-            stack.push(state)
-
-        elif node.has_right_child() and not state.get_visited_right():
-            state.set_visited_right()
-            node = node.get_right_child()
-            visit_order.append(node.get_value())
-            state = State(node)
-
-        else:
-            stack.pop()
-            if not stack.is_empty():
-                state = stack.top()
-                node = state.get_node()
-            else:
-                node = None
+    q.enq(node)
+    def traverse():
+        node = q.deq()
+        visit_order.append(node)
+        if node.has_left_child():
+            q.enq(node.get_left_child())
+        if node.has_right_child():
+            q.enq(node.get_right_child())
             
-    if debug_mode:
-            print(f"""
-loop count: {count}
-current node: {node}
-stack:
-{stack}
-            """)
     return visit_order
